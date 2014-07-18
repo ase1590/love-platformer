ba07c5b8c353dbec0ed4d3729224f1b691bff467
ase1590 <ase1590@hotmail.com>
2014-07-18 14:29:41 -0500

    show font on screen

--- toggletest/main.lua
+++ toggletest/main.lua
@@ -1,15 +1,33 @@
 function love.load()
 
+
+-- set up some fonts
+	small = love.graphics.newFont(25)
+	medium = love.graphics.newFont(45)
+	large = love.graphics.newFont(72)
+
+
+
 end
 
 
 
 function love.draw()
 
+	love.graphics.setFont(small)
+	love.graphics.setColor(255,255,255, 200)
+--	love.graphics.print("PONG", 300, 10)
+
 end
 
 
 
 function love.update()
+
+end
+
+
+
+function love.keyboard.keypressed(key)
 
 end
