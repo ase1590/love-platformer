18a9faaf39c83435bf4a868da3687e82439e33be
ase1590 <ase1590@hotmail.com>
2014-07-18 15:06:45 -0500

    made a terrible on screen counter

--- toggletest/main.lua
+++ toggletest/main.lua
@@ -1,11 +1,10 @@
 function love.load()
-
-
+	counter = 2
+	text = "test"
 -- set up some fonts
 	small = love.graphics.newFont(25)
 	medium = love.graphics.newFont(45)
 	large = love.graphics.newFont(72)
-
 
 
 end
@@ -16,7 +15,7 @@ function love.draw()
 
 	love.graphics.setFont(small)
 	love.graphics.setColor(255,255,255, 200)
---	love.graphics.print("PONG", 300, 10)
+	love.graphics.print(text, 300, 100)
 
 end
 
@@ -24,10 +23,17 @@ end
 
 function love.update()
 
+counter = counter+1
+
+
 end
 
 
 
-function love.keyboard.keypressed(key)
-
+function love.keypressed(key)
+   if key == 'b' then
+      text = ""
+   elseif key == 'a' then
+      text = counter
+   end
 end
