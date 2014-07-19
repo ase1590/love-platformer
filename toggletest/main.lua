9c6f0d8d538d653bdd045ca30ca70497c47cf1bb
ase1590 <ase1590@hotmail.com>
2014-07-18 21:57:49 -0500

    copied in working toggle test

--- toggletest/main.lua
+++ toggletest/main.lua
@@ -1,4 +1,14 @@
+﻿require "toggletext"
+
+
 function love.load()
+	counter = 2
+	
+-- set up some fonts
+	small = love.graphics.newFont(25)
+	medium = love.graphics.newFont(45)
+	large = love.graphics.newFont(72)
+
 
 end
 
@@ -6,10 +16,20 @@ end
 
 function love.draw()
 
+	love.graphics.setFont(small)
+	love.graphics.setColor(255,255,255, 200)
+	toggletext()
+
 end
 
 
 
 function love.update()
 
+counter = counter+1
+
+
 end
+
+
+
